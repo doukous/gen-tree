2c84e5a58620c0739408c773036e7ac4e6c3b78d
mohamed doucoure <mohamed.doucoure@protonmail.com>
2025-04-20 23:39:50 +0000

    added restframework

--- src/genviz/urls.py
+++ src/genviz/urls.py
@@ -3,7 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.HoweView.as_view(), name='home'),
     path('families', views.CreateFamilyView.as_view(), name='new-family'),
     path('registration-completed', views.RegistrationCompleteView.as_view(), name='register-family'),
 ]
