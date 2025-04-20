ae57c53d6a29a8d3a01b72ab514b9dbde9a7e88a
mohamed doucoure <mohamed.doucoure@protonmail. com>
2025-04-20 20:56:25 +0000

    complete form validation checking

--- src/genviz/forms.py
+++ src/genviz/forms.py
@@ -17,7 +17,7 @@ class PersonForm(forms.Form):
         ('awa', 'Awa')
     }
 
-    firstname = forms.CharField()
+    firstname = forms.CharField(required=False)
     birth_date = forms.DateField(
         widget=forms.DateInput(attrs={
            'type': 'date',
@@ -44,16 +44,35 @@ class ChildForm(PersonForm):
     sex = forms.ChoiceField(choices=SEX_CHOICES, initial='male', required=False)
     role = 'child'
 
-ChildFormSet = forms.formset_factory(ChildForm, extra=0)
+ChildFormSet = forms.formset_factory(ChildForm, extra=1)
 
 
 class FamilyForm(forms.Form):
     def __init__(self, data=None, *args, **kwargs):
         super().__init__(data, *args, **kwargs)
 
-        self.new_first_partner = PersonForm(data=data, prefix='first_partner')
-        self.new_second_partner = PersonForm(data=data, prefix='second_partner')
-        self.children = ChildFormSet(data, prefix='children')
+        self.nested_data = {
+            'new_first_partner': None,
+            'new_second_partner': None,
+            'new_children': None
+        }
+
+        if data:
+            self.nested_data = {}
+
+            children_data = {key:data[key] for key in data.keys() if key.startswith('new_children')}
+            self.nested_data['new_children'] = children_data
+
+            first_partner_data = {key:data[key] for key in data.keys() if key.startswith('new_first_partner')}
+            self.nested_data['new_first_partner'] = first_partner_data
+
+            second_partner_data = {key:data[key] for key in data.keys() if key.startswith('new_second_partner')}
+            self.nested_data['new_second_partner'] = second_partner_data
+
+            
+        self.new_first_partner = PersonForm(data=self.nested_data['new_first_partner'], prefix='new_first_partner')
+        self.new_second_partner = PersonForm(data=self.nested_data['new_second_partner'], prefix='new_second_partner')
+        self.children = ChildFormSet(data=self.nested_data['new_children'], prefix='new_children')
 
     family_name = forms.CharField(initial='family name')
 
@@ -72,15 +91,32 @@ class FamilyForm(forms.Form):
 
     children_choices = forms.MultipleChoiceField(
         choices=all_people, 
-        required=False,
-        initial=[""]
+        initial=[''],
+        required=False
     )
 
     def is_valid(self):
         valid = super().is_valid()
-
         partners_valid = self.new_first_partner.is_valid() and self.new_second_partner.is_valid()
         children_valid = self.children.is_valid()
 
         return valid and partners_valid and children_valid
+    
+    def clean(self):
+        cleaned_data = super().clean()
+        self.new_first_partner.is_valid()
+        self.new_second_partner.is_valid()
+
+        if not (self.cleaned_data['first_partner_choice'] or self.new_first_partner.cleaned_data['firstname']) \
+            and not (self.cleaned_data['second_partner_choice'] or self.new_second_partner.cleaned_data['firstname']):
+
+            raise forms.ValidationError('at least one partner need to be defined')
+        
+        elif self.cleaned_data['first_partner_choice'] and self.new_first_partner.cleaned_data['firstname']:
+            raise forms.ValidationError('cannot set first partner choice and create a new first partner at the same time')
+    
+        elif self.cleaned_data['second_partner_choice'] and self.new_second_partner.cleaned_data['firstname']:
+            raise forms.ValidationError('cannot set second partner choice and create a new second partner at the same time')
+
+        return cleaned_data
     
