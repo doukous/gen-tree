531755570677321681bdec78404a3812a6398a79
mohamed doucoure <mohamed.doucoure@protonmail.com>
2025-05-27 17:39:17 +0000

    add the form for the login & delete the tailwind output file

--- gentree/auth.py
+++ gentree/auth.py
@@ -1,17 +1,26 @@
 from flask import Blueprint, redirect, render_template, request, session, url_for
+from flask_wtf import FlaskForm
+from wtforms import PasswordField, StringField
+from wtforms.validators import DataRequired
 
 
 bp = Blueprint('auth', __name__, url_prefix="/auth")
 
 
+class LoginForm(FlaskForm):
+    email = StringField('email', validators=[DataRequired()])
+    password = PasswordField('password', validators=[DataRequired()])
+
+
 @bp.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'GET':
-        return render_template('auth/login.html')
+        form = LoginForm()
+        return render_template('auth/login.html', form=form)
     
     else:
-        username = request.form['username']
-        session['username'] = username
+        user_mail = request.form['email']
+        user_password = request.form['password']
 
         if 'next' in request.args:
             return redirect(request.args['next'])
