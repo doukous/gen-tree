d1c64a3113890abeacd5d43002884dd20bad25b6
mohamed doucoure <mohamed.doucoure@protonmail.com>
2025-06-04 18:05:46 +0000

    re-implement the subforms for male partner and female partner

--- gentree/gentree_viz/forms.py
+++ gentree/gentree_viz/forms.py
@@ -2,16 +2,66 @@ from flask_wtf import FlaskForm
 from wtforms import FormField, StringField, DateField, SelectField
 from wtforms.validators import DataRequired
 from gentree.db import db
+from gentree.gentree_viz.schemas import FamilyChoice
 
 
 class Choices:
-    def __init__(self, gentree_id):
-        self.gentree_id = gentree_id
-
-    @property
-    def family_choices(self):
+    @classmethod
+    def get_people_choices(cls, gentree_id, sex=None):
         driver = db.driver
 
+        results = None
+
+        if sex:
+            results, _, _ = driver.execute_query(
+            """
+            MATCH (f: FamilyTree)-[:BELONGS_TO]->(: GenealogicalTree { uid: $uid })
+            CALL (f) {
+                MATCH (p: Person {sex: $sex})-[:IS_MEMBER_OF {status: "child"}]->(f)
+                RETURN collect({
+                    id: p.uid,
+                    firstname: p.firstname,
+                    sex: p.sex
+                }) as members
+            }
+            RETURN f.name AS family_name, f.uid AS family_id, members
+            """,
+            database_='gentree',
+            uid=str(gentree_id),
+            sex=sex
+        )
+        
+        else:
+            results, _, _ = driver.execute_query(
+                """
+                MATCH (f: FamilyTree)-[:BELONGS_TO]->(: GenealogicalTree { uid: $uid })
+                CALL (f) {
+                    MATCH (p: Person)-[:IS_MEMBER_OF {status: "child"}]->(f)
+                    RETURN collect({
+                        id: p.uid,
+                        firstname: p.firstname,
+                        sex: p.sex
+                    }) as members
+                }
+                RETURN f.name AS family_name, f.uid AS family_id, members
+                """,
+                database_='gentree',
+                uid=str(gentree_id)
+            )
+
+        results = [
+            FamilyChoice.model_validate(result.data()) 
+            for result in results
+        ]
+
+        choices = {}
+
+        for result in results:
+            (key, value), = result.model_dump().items()
+            choices[key] = value
+        
+        return choices
+    
 
 class PersonForm(FlaskForm):
     firstname = StringField('Firstname')
@@ -24,15 +74,17 @@ class ChildrenForm(PersonForm):
 
 
 class PartnerForm(PersonForm):
-    driver = db.get_driver()
+    driver = db.driver
 
     family_choice = SelectField('Family choice')
-    partner_choice = SelectField('Choosen partner')
-
+    partner_choice = SelectField('Choosen partner', choices=[])  
 
 
 class FamilyTreeForm(FlaskForm):
     name = StringField('Family name', validators=[DataRequired()])
+    
+    male_existing_choices = SelectField('Choices')
+    female_existing_choices = SelectField('Choices')
 
     new_male_partner = FormField(PersonForm)
     new_female_partner = FormField(PersonForm)
--- gentree/gentree_viz/views.py
+++ gentree/gentree_viz/views.py
@@ -1,6 +1,6 @@
 from flask import flash, g, redirect, render_template, request
 import neo4j
-from gentree.gentree_viz.forms import FamilyTreeForm
+from gentree.gentree_viz.forms import Choices, FamilyTreeForm
 from gentree.utils import login_required
 from . import genviz
 from gentree.db import db
@@ -33,8 +33,14 @@ def get_tree(gentree_id):
 
 @genviz.route('/<uuid:gentree_id>/family-trees', methods=['GET', 'POST'])
 def new_family_tree(gentree_id):
-    if request.method == 'GET':
+    if request.method == 'GET':    
         form = FamilyTreeForm()
+        male_existing_people = Choices.get_people_choices(gentree_id, sex='male')
+        women_existing_people = Choices.get_people_choices(gentree_id, sex='female')
+        
+        form.male_existing_choices.choices = male_existing_people
+        form.female_existing_choices.choices = women_existing_people
+
         return render_template('gentree_viz/forms/new-family-form.html', gentree_id=gentree_id, form=form)
 
     else:
