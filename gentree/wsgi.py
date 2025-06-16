49a94768d443056c88da072b0ee88197a3d3758a
mohamed doucoure <mohamed.doucoure@protonmail.com>
2025-06-16 22:38:31 +0000

    correct the root parameter for the indexing of static directory for whitenoise config

--- gentree/wsgi.py
+++ gentree/wsgi.py
@@ -2,4 +2,4 @@ from gentree import create_app
 from whitenoise import WhiteNoise
 
 app = create_app()
-app.wsgi_app = WhiteNoise(app.wsgi_app, root='static/', prefix='static/')
+app.wsgi_app = WhiteNoise(app.wsgi_app, root='gentree/static/', prefix='static')
